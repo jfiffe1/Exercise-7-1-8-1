93dc452d1eca5eead6575d1a4d2366cb65328f19
jansfh85 <jansfh85@gmail.com>
2026-02-25 00:53:47 -0500

    Add project files.

---
+++ Exercise 8-1/Ch08Ex1GuitarShop/Ch08Ex1GuitarShop/GuitarShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GuitarShop.Models;
+
+namespace GuitarShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ShopContext context;
+        private List<Category> categories;
+
+        public ProductController(ShopContext ctx)
+        {
+            context = ctx;
+            categories = context.Categories
+                    .OrderBy(c => c.CategoryID)
+                    .ToList();
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Route("[area]/[controller]s/{id?}")]
+        public IActionResult List(string id = "All")
+        {
+            List<Product> products;
+            if (id == "All")
+            {
+                products = context.Products
+                    .OrderBy(p => p.ProductID).ToList();
+            }
+            else
+            {
+                products = context.Products
+                    .Where(p => p.Category.Name == id)
+                    .OrderBy(p => p.ProductID).ToList();
+            }
+
+            // Pasos 15 y 16: Creamos el modelo y lo pasamos a la vista
+            var model = new ProductListViewModel
+            {
+                Categories = categories,
+                Products = products,
+                SelectedCategory = id
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // create new Product object
+            Product product = new Product();
+
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Add";
+            ViewBag.Categories = categories;
+
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            // get Product object for specified primary key
+            Product product = context.Products
+                .Include(p => p.Category)
+                .FirstOrDefault(p => p.ProductID == id) ?? new Product();
+
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Update";
+            ViewBag.Categories = categories;
+
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ProductID == 0)           // new product
+                {
+                    context.Products.Add(product);
+                    // Paso 24: TempData message for adding
+                    TempData["UserMessage"] = $"You just added the product {product.Name}";
+                }
+                else                                  // existing product
+                {
+                    context.Products.Update(product);
+                    // Paso 25: TempData message for updating
+                    TempData["UserMessage"] = $"You just updated the product {product.Name}";
+                }
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Action = "Save";
+                ViewBag.Categories = categories;
+                return View("AddUpdate", product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Product product = context.Products
+                .FirstOrDefault(p => p.ProductID == id) ?? new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
